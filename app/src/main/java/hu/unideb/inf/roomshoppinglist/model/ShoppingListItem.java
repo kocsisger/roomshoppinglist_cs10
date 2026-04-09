270a2fe618dd138a1bee440d1f769d4632decc91
Peter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2026-04-09 10:47:35 +0200

    POJO

---
+++ app/src/main/java/hu/unideb/inf/roomshoppinglist/model/ShoppingListItem.java
@@ -0,0 +1,27 @@
+package hu.unideb.inf.roomshoppinglist.model;
+
+public class ShoppingListItem {
+    int id;
+    String name;
+
+    @Override
+    public String toString() {
+        return id + " - " + name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
